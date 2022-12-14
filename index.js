b0712c8bc13ff5bdb013b9615daac7c0b6aa5327
Vsoto7697 <109629515+Vsoto7697@users.noreply.github.com>
2022-12-13 20:56:50 -0500

    Begin server after DB association

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 // Definitions and Requirements for Variables
 const inquirer = require('inquirer');
 const db = require('./db/connection');
+
+// Begin server after DB association
+db.connect(err => {
+    if (err) throw err;
+    console.log('Database connected.');
+    employee_tracker();
+});
