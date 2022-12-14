6f0b8527491e76d6ce6afc673edc26fb5c4cd6ce
Vsoto7697 <109629515+Vsoto7697@users.noreply.github.com>
2022-12-13 20:24:53 -0500

    created a role table

--- db/schema.sql
+++ db/schema.sql
@@ -3,3 +3,11 @@ CREATE TABLE departments (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30)
 );
+
+-- create role table 
+CREATE TABLE role (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,AUTO_INCREMENT,
+    department_id INTEGER
+);
