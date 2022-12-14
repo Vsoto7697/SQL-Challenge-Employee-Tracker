d5ea986ebb858f32e9ff026624a4bda416718e1a
Vsoto7697 <109629515+Vsoto7697@users.noreply.github.com>
2022-12-13 20:42:12 -0500

    Adds employee roles to the role table and employee data to the employee tab

--- db/seeds.sql
+++ db/seeds.sql
@@ -8,3 +8,19 @@ VALUES
   ('Legal');
 
 -- Adds employee roles to the role table
+INSERT INTO role
+  (title, salary, department_id)
+VALUES
+  ('Software Engineer', 120000, 1),
+  ('Salesassociate', 65000, 2),
+  ('Accountant', 91000, 3),
+  ('Lawyer', 100000, 4);
+
+-- Adds employee data to the employee tab
+INSERT INTO employee
+  (first_name, last_name, role_id, manager_id)
+VALUES
+  ('Victor', 'Soto', 1, 4),
+  ('Seth', 'Mapau', 2, 3),
+  ('Jeff', 'Jacquez', 3, 1),
+  ('Sade', 'Singh', 4, 5);
